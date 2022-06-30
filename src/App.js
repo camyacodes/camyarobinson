a3f0f75b7b1698a561bd37433b8b8ebc08ec7768
Camya <camyacodes@gmail.com>
2022-06-30 15:43:50 -0400

    chaning homepage route

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <NavbarComp></NavbarComp>
         <div className='content'>
           <Switch>
-            <Route exact path='/'>
+            <Route exact path='/camyarobinson'>
               <Home></Home>
             </Route>
             <Route path='/about'>
